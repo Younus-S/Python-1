c87482f3d4486955a91c7b7780e02a5449398ed6
Younus-S <sulimanyyounus@gmail.com>
2023-07-15 19:27:43 +0500

    Mathematical Operations and Age

--- operations.py
+++ operations.py
@@ -5,3 +5,9 @@ print(CarModel-CarModel)
 print(CarModel*CarModel)
 print(CarModel/CarModel)
 print(University,CarModel)
+
+print("My Age in Seconds:", 31*365*24*60*60)
+
+print("My Age in Minutes:", 31*365*24*60)
+
+print("My Age in Hours:", 31*365*24)
