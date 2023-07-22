aadf29c8619b134c04b74ebeea39d1a4834850bc
Younus-S <sulimanyyounus@gmail.com>
2023-07-22 20:02:09 +0500

    list and operators classwork added

---
+++ AssigmentOperators.py
@@ -0,0 +1,30 @@
+print(87+99)
+print(87-99)
+print(87*99)
+print(87/99)
+print(2**5)
+print(87//99)
+print()
+print(2000%99)
+print()
+a=5
+a+=9
+print(a)
+a-=9
+print(a)
+a*=9
+print(a)
+a/=9
+print(a)
+a**=9
+print(a)
+a//=9
+print(a)
+a%=9
+print(a)
+
+x=7
+y=7
+b=y
+print(x is b)
+print(b is y)
---
+++ List.py
@@ -0,0 +1,22 @@
+province=["Kabul","Herat","Qandahar","Daikundi","Ghazni"]
+print(province[3])
+print(province[:3])
+print(province[1:4])
+print(province[2:])
+print(province[-1])
+print(province[-3:])
+print(province[-3:-1])
+print()
+print(province[-2:1])
+print(province[2:-1])
+print(province[-1:4])
+
+if "Kabul" in province:
+    print("True")
+print()
+
+time=[365,24,60,60]
+for t in time:
+    print(t)
+    print(t/2)
+    print("myself")
